3a9b696ac754c1f7bae29dcbc8d060171cd44cd5
Devon Weems <devonweems@Devons-MacBook-Air.local>
2020-09-15 14:02:42 -0400

    Completed Application
    Completed burger app. No styling .

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,13 @@
-var mysql = require("mysql");
-
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "myhhyh-Moghod-3pubma",
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id: " + connection.threadId);
-});
-
+const mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "myhhyh-Moghod-3pubma",
+    database: "burgers_db"
+  });
+}
+connection.connect();
 module.exports = connection;
